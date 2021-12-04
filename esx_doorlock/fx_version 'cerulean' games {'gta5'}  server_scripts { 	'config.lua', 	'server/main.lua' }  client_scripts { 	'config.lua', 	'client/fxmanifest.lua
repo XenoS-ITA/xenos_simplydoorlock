1abdf47bdbe5b365554699efc1299d356f816b86
XenoXzTV <90718666+XenoXzTV@users.noreply.github.com>
2021-12-04 22:05:35 +0100

    add fxmanifest

---
+++ esx_doorlock/fx_version 'cerulean' games {'gta5'}  server_scripts { 	'config.lua', 	'server/main.lua' }  client_scripts { 	'config.lua', 	'client/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
+
+dependency 'es_extended'
